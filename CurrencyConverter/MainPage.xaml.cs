bb63b04cd02efa9a17738f96ce2bfb0c86cfe329
VaNek2001 <ya.kurochkin2001@yandex.ru>
2022-10-13 20:05:15 +0500

    v 0.2

--- CurrencyConverter/MainPage.xaml.cs
+++ CurrencyConverter/MainPage.xaml.cs
@@ -1,24 +1,77 @@
-﻿namespace CurrencyConverter;
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Xml;
+
+namespace CurrencyConverter;
 
 public partial class MainPage : ContentPage
 {
+    public string ChCode;
 
-	public MainPage()
-	{
-		InitializeComponent();
-        
+    private static int size = 0;
 
-		LabelDate.Text = "Курс на " + DP.Date.ToString("d");
-	}
+    public int index = 0;
 
-    private void GetValCurs()
+    public double[] arrValue;
+
+    public MainPage()
     {
-        
+        InitializeComponent();
+
+        String URLString = "https://www.cbr-xml-daily.ru/daily_utf8.xml";
+        XmlTextReader reader = new XmlTextReader(URLString);
+
+        Curs_1.Items.Add("Российский рубль (RUB)");
+        Curs_2.Items.Add("Российский рубль (RUB)");
+
+        size++;
+
+        while (reader.Read())
+        {
+            if (reader.IsStartElement() && reader.Name == "CharCode")
+            {
+                reader.Read();
+                ChCode = reader.Value;
+            }
+
+            if (reader.IsStartElement() && reader.Name == "Name")
+            {
+                reader.Read();
+                Curs_1.Items.Add(reader.Value + " (" + ChCode + ")");
+                Curs_2.Items.Add(reader.Value + " (" + ChCode + ")");
+
+                size++;
+            }
+        }
+
+        XmlTextReader reader_2 = new XmlTextReader(URLString);
+
+        arrValue = new double[size+1];
+
+        arrValue[index] = 1;
+
+        index++;
+
+        while (reader_2.Read())
+        {
+            if (reader_2.IsStartElement() && reader_2.Name == "Value")
+            {
+                reader_2.Read();
+                arrValue[index] = double.Parse(reader_2.Value.Replace('.',','));
+                xml_Label.Text += arrValue[index] + "\n";
+                index++;
+            }
+        }
+
+        LabelDate.Text = "Курс на " + DP.Date.ToString("d");
+
+        Curs_1.SelectedIndex = 11;
+        Curs_2.SelectedIndex = 0;
     }
 
     void DateSelected(System.Object sender, Microsoft.Maui.Controls.DateChangedEventArgs e)
     {
-		LabelDate.Text = "Курс на " + e.NewDate.ToString("d");
+        LabelDate.Text = "Курс на " + e.NewDate.ToString("d");
     }
 
     void PickerSelectedIndexChanged(System.Object sender, System.EventArgs e)
@@ -27,11 +80,31 @@ public partial class MainPage : ContentPage
 
     void EntryTextChanged_1(System.Object sender, Microsoft.Maui.Controls.TextChangedEventArgs e)
     {
+        if (EntryNote_1.Text != "")
+        {
+            double currCurs = arrValue[Curs_1.SelectedIndex] / arrValue[Curs_2.SelectedIndex];
+            EntryNote_2.TextChanged -= EntryTextChanged_2;
+            EntryNote_2.Text = Math.Round((currCurs * double.Parse(EntryNote_1.Text)), 3).ToString();
+            EntryNote_2.TextChanged += EntryTextChanged_2;
+        }
+        else
+        {
+            EntryNote_1.Text = "";
+        }
     }
 
     void EntryTextChanged_2(System.Object sender, Microsoft.Maui.Controls.TextChangedEventArgs e)
     {
+        if (EntryNote_2.Text != "")
+        {
+            double currCurs = arrValue[Curs_2.SelectedIndex] / arrValue[Curs_1.SelectedIndex];
+            EntryNote_1.TextChanged -= EntryTextChanged_1;
+            EntryNote_1.Text = Math.Round((currCurs * double.Parse(EntryNote_2.Text)), 3).ToString();
+            EntryNote_1.TextChanged += EntryTextChanged_1;
+        }
+        else
+        {
+            EntryNote_2.Text = "";
+        }
     }
 }
-
-
